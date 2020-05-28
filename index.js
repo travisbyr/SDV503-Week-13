029da9261d3fc49b5ae7f1a9ca9abfedd23dc3a3
Travis <Travis-Byrman@live.nmit.ac.nz>
2020-05-29 11:02:18 +1200

    Created a calculator using classes

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 // Friday 29th May - Classes
+// class Student {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
+
+// let firstName = new Student("Travis", "Byrman");
+// let lastName = new Student("Ali", "Kahwaji");
+
+// console.log(firstName);
+// console.log(lastName);
+
 class Student {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, year) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.year = year;
+  }
+  yourEmail() {
+    return `Your email address is ${this.firstName.toLowerCase()}-${this.lastName.toLowerCase()}@nmit.ac.nz`;
   }
 }
 
-let firstName = new Student("Travis", "Byrman");
-let lastName = new Student("Ali", "Kahwaji");
+let firstStudent = new Student("Travis", "Byrman", 2020);
+console.log(firstStudent);
 
-console.log(firstName);
-console.log(lastName);
+let secondStudent = new Student("Travis", "Byrman");
+console.log(secondStudent.yourEmail());
+
+// Class Template
+class DataStructure {
+  constructor() {
+    // what default properties should it have?
+  }
+  someInstanceMethod() {
+    // what should each object created from this class be able to do?
+  }
+}
+
+//class calculator, take 2 properties, add, sub, mult and divide
+
+class Calculator {
+  constructor(numberOne, numberTwo) {
+    this.numberOne = numberOne;
+    this.numberTwo = numberTwo;
+  }
+  addition() {
+    return this.numberOne + this.numberTwo;
+  }
+  subtraction() {
+    return this.numberOne - this.numberTwo;
+  }
+  multiplication() {
+    return this.numberOne * this.numberTwo;
+  }
+  division() {
+    return this.numberOne / this.numberTwo;
+  }
+}
+
+let firstCalculation = new Calculator(1, 2);
+
+console.log(firstCalculation.addition());
+console.log(firstCalculation.subtraction());
+console.log(firstCalculation.multiplication());
+console.log(firstCalculation.division());
