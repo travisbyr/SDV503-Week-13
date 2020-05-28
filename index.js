ddc07c0bd937c1db4a40b3efabde09a928037109
Travis <Travis-Byrman@live.nmit.ac.nz>
2020-05-29 09:56:36 +1200

    Class example

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//index.js
+// Friday 29th May - Classes
+class Student {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+let firstName = new Student("Travis", "Byrman");
+let lastName = new Student("Ali", "Kahwaji");
+
+console.log(firstName);
+console.log(lastName);
