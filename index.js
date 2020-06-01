2deb9f70fe6dd98d227e37bcb143d8ceb93f442a
Travis <Travis-Byrman@live.nmit.ac.nz>
2020-06-02 10:00:28 +1200

    Created linked list push/pop example

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
 // console.log(firstName);
 // console.log(lastName);
-
+/*
 class Student {
   constructor(firstName, lastName, year) {
     this.firstName = firstName;
@@ -78,3 +78,58 @@ var queue = [1, 2, 3];
 console.log(queue);
 queue.shift();
 console.log(queue);
+*/
+
+
+// Linked List Example
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if(!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while(current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if(this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
+
+var list = new SinglyLinkedList()
+list.push("Hi")
+list.pop("Hi")
+
+console.log(list)
+
